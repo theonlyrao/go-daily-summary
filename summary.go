abb2292189ab16f6dd403175f0ba65f1fd969f20
Ashwin Rao <theonlyrao@gmail.com>
2019-07-26 10:28:39 -0500

    Use channels and select to wait for responses

--- summary.go
+++ summary.go
@@ -2,29 +2,40 @@ package main
 
 import (
 	"fmt"
-	"sync"
 )
 
+func showLatestTweet(tweet, time chan string) {
+	w, i := GetLatestTweet()
+	tweet <- w
+	time <- i
+}
+
+func showWeather(current, later chan string) {
+	c, l := GetWeather()
+	current <- c.Summary
+	later <- l.Summary
+}
+
 func main() {
-        var wg sync.WaitGroup
+	current := make(chan string)
+	later := make(chan string)
 
-	wg.Add(1)
-	go func() {
-		tweet, time := GetLatestTweet()
-		fmt.Println("Pres. Trump says: " + tweet)
-		fmt.Println(time)
-		fmt.Println()
-		wg.Done()
-	} ()
+	tweet := make(chan string)
+	time := make(chan string)
 
-	wg.Add(1)
-	go func() {
-		cur, lat := GetWeather()
-		fmt.Println(cur.Summary)
-		fmt.Println(lat.Summary)
-		fmt.Println()
-		wg.Done()
-	} ()
-
-	wg.Wait()
+	go showLatestTweet(tweet, time)
+	go showWeather(current, later)		
+	
+	for x := 0; x < 4; x++ {
+		select {
+		case w := <- tweet:
+			fmt.Println(w)
+		case i := <- time:
+			fmt.Println(i)
+		case c := <- current:
+			fmt.Println(c)
+		case l := <- later:
+			fmt.Println(l)
+		}
+	}
 }
